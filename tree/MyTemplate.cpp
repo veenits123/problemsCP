163cdfc4bfed0bc29d4876ebc6a3db4606453867
Veenits123 <123sveenit@gmail.com>
2021-03-28 14:09:43 +0530

    spaces and tabs

--- tree/MyTemplate.cpp
+++ tree/MyTemplate.cpp
@@ -15,7 +15,7 @@
                                                    \/  */             
                 
 // Author : vee_nits123
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long int
